f8ab40a46294051348ee97e299b0be3b2fd379e3
Matthew Korolyov <korolyofff@MacBook-Pro-Matthew.local>
2020-10-22 18:05:31 +0500

    Test

--- scraper/scraper_engine/followers_collector.py
+++ scraper/scraper_engine/followers_collector.py
@@ -64,11 +64,8 @@ class Cliker:
         self.driver.execute_script("arguments[0].click();", login)
 
     def find_profile(self, profile):
-        try:
-            WebDriverWait(self.driver, 60).until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')))
-        except TimeoutException:
-            raise TimeoutException
+        WebDriverWait(self.driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')))
 
         self.driver.get('https://www.instagram.com/{}/'.format(profile))
         sleep(2)
